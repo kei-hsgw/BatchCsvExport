b567e5f6da5a9c84ae17c949b453cbc25054b83b
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-25 11:38:58 +0900

    エクスポートの結合テスト準備

---
+++ src/test/java/com/example/demo/integration/CsvExportIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CsvExportIntegrationTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
